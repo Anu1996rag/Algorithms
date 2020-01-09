9f6a3a23f2bcbde896e98eb53bcd7d62cecd85cb
Anurag Patil <49953149+Anu1996rag@users.noreply.github.com>
2020-01-09 10:18:15 +0530

    Add files via upload
    Quick Sort Algorithm with the use of recursion

---
+++ quick_sort.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  9 10:08:31 2020
+
+@author: Gurudas
+
+Quick Sort Algorithm
+"""
+
+def quicksort(arr):
+    if len(arr) < 2:
+        return arr  #base case ; arrays with 0 or 1 elements are already sorted
+    else:
+        pivot = arr[0] # recursive case
+        
+        less = [i for i in arr[1:] if i <=  pivot] # sub- array of all the elements less than the pivot element
+        
+        greater = [i for i in arr[1:] if i > pivot] # sub-array of all the elements greater than pivot element 
+        
+        return quicksort(less) + [pivot] + quicksort(greater) # clubbing the arrays together after sorting 
+    
+print(quicksort([10,5,2,3]))
+
