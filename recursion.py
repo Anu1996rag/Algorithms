ff0d74cc55b8c5de0f0f51a3d9e1d816dad51688
Anurag Patil <49953149+Anu1996rag@users.noreply.github.com>
2020-01-09 10:07:47 +0530

    Update recursion.py

--- recursion.py
+++ recursion.py
@@ -28,3 +28,30 @@ def factorial(n):
     
 print(factorial(6))
 
+#calculating sum 
+
+def summation(arr):
+    if arr == []:
+        return 0
+    return arr[0] + summation(arr[1:])
+
+print(summation([1,2,3]))
+    
+#to count the number of items in the list 
+
+def count(lists):
+    if lists == []:
+        return 0
+    return 1 + count(lists[1:])
+
+print(count([1,2,3,4,5]))
+
+#to find out the maximum number in the list
+
+def maximum(arr):
+    if len(arr) == 2:
+        return arr[0] if arr[0] > arr[1] else arr[1]
+    sub_max = maximum(arr[1:])
+    return arr[0] if arr[0] > sub_max else sub_max
+
+print(maximum([2,5,4,1]))
